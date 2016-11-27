806842bcaa4a428ac6930d8bac52ef2c1f834c8d
FalconPilot <fpilot.github@gmail.com>
2016-11-27 14:08:08 +0100

    added standard project

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+**  Button
+*/
+
+document.addEventListener("DOMContentReady", function() {
+  let button = document.getElementById('namegen');
+  button.addEventListener("click", function() {
+    console.log("Coucou");
+  });
+});
